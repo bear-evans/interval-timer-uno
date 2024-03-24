af464df2b11cb57c780e0f053f196c4db7a20bbb
Bear Evans <bear-evans@outlook.com>
2024-03-24 18:59:40 -0400

    Basic settings page and functions

--- IntervalTimer_Uno/Presentation/SettingsPage.cs
+++ IntervalTimer_Uno/Presentation/SettingsPage.cs
@@ -3,6 +3,75 @@ namespace IntervalTimer_Uno.Presentation;
 /// <summary> View for the settings management screen. </summary>
 public sealed partial class SettingsPage : Page
 {
+    #region Design Variables
+
+    // ---------------------------------------------
+    private readonly float settingSpacing = 16;
+    private readonly float settingMargin = 0;
+    private readonly float settingHeight = 60;
+    // ---------------------------------------------
+
+    #endregion Design Variables
+
+    #region Design Snippets
+
+    // ---------------------------------------------
+    /// <summary> Template for a setting entry wrapper panel. </summary>
+    private StackPanel SettingPanel
+    {
+        get
+        {
+            return new StackPanel()
+                        .Orientation(Orientation.Horizontal)
+                        .Spacing(50)
+                        .Margin(settingMargin)
+                        .HorizontalAlignment(HorizontalAlignment.Center);
+        }
+    }
+
+    private TimePicker TimeSettingPicker
+    {
+        get
+        {
+            return new TimePicker()
+                .Height(settingHeight);
+        }
+    }
+
+    /// <summary> Template for a typed time input box. </summary>
+    private TextBox IntervalInputBox
+    {
+        get
+        {
+            TextBox box = new TextBox();
+            InputScope scope = new InputScope();
+            InputScopeName scopeName = new InputScopeName();
+            scopeName.NameValue = InputScopeNameValue.NumberFullWidth;
+            scope.Names.Add(scopeName);
+
+            box.InputScope = scope;
+
+            box.Height = settingHeight;
+            return box;
+        }
+    }
+
+    /// <summary> Template for the : separators between interval input boxes. </summary>
+    private TextBlock TimeSeperator
+    {
+        get
+        {
+            return new TextBlock()
+                .Text(" : ")
+                .VerticalAlignment(VerticalAlignment.Center)
+            ;
+        }
+    }
+    // ---------------------------------------------
+
+    #endregion Design Snippets
+
+    // =============================================================
     public SettingsPage()
     {
         this.DataContext<SettingsViewModel>((page, vm) => page
@@ -19,15 +88,35 @@ public sealed partial class SettingsPage : Page
                             .VerticalAlignment(VerticalAlignment.Top)
                             .Spacing(16)
                             .Children(
-                                new StackPanel().Orientation(Orientation.Horizontal)
-                                    .Spacing(50)
+                                new StackPanel()
+                                    .Grid(row: 1, column: 1)
                                     .HorizontalAlignment(HorizontalAlignment.Center)
+                                    .VerticalAlignment(VerticalAlignment.Top)
+                                    .Spacing(settingSpacing)
                                     .Children(
-                                        new TextBlock().Text("Start Time"),
-                                        new TimePicker()
+                                        SettingPanel
+                                            .Children(
+                                                new TextBlock().Text("Start Time"),
+                                                TimeSettingPicker.Time(x => x.Bind(() => vm.StartTime).TwoWay())
+                                            ),
+                                        SettingPanel
+                                            .Children(
+                                                new TextBlock().Text("End Time"),
+                                                TimeSettingPicker.Time(x => x.Bind(() => vm.EndTime).TwoWay())
+                                            ),
+                                        SettingPanel
+                                            .Children(
+                                                new TextBlock().Text("Interval Period"),
+                                                IntervalInputBox,
+                                                TimeSeperator,
+                                                IntervalInputBox,
+                                                TimeSeperator,
+                                                IntervalInputBox
+                                            )
                                     )
+                            )
                     )
             )
-        ));
+        );
     }
 }
--- IntervalTimer_Uno/Presentation/SettingsViewModel.cs
+++ IntervalTimer_Uno/Presentation/SettingsViewModel.cs
@@ -6,10 +6,10 @@ public partial class SettingsViewModel : ObservableObject
     #region External References
 
     // ------------------------------------------
-    //private INavigator navigator;
-    //private IOptions<AppConfig> appConfig;
-    //private IStringLocalizer localizer;
-    //private IWritableOptions<TimeInputConfig> timeInputConfig;
+    private INavigator navigator;
+    private IOptions<AppConfig> appConfig;
+    private IStringLocalizer localizer;
+    private IWritableOptions<TimeInputConfig> timeInputConfig;
     // ------------------------------------------
 
     #endregion External References
@@ -17,32 +17,117 @@ public partial class SettingsViewModel : ObservableObject
     #region Settings
 
     // ------------------------------------------
-    //[ObservableProperty] private TimeSpan startTime;
-    //[ObservableProperty] private TimeSpan endTime;
-    //[ObservableProperty] private TimeSpan interval;
+    [ObservableProperty] private TimeSpan startTime;
+    [ObservableProperty] private TimeSpan endTime;
+    [ObservableProperty] private TimeSpan interval;
     // ------------------------------------------
 
     #endregion Settings
+
+    #region Input Fields
+
+    // ------------------------------------------
+    [ObservableProperty] private int intervalHourField;
+    [ObservableProperty] private int intervalMinuteField;
+    [ObservableProperty] private int intervalSecondField;
+
+    /// <summary> Event fired when an interval input field changes. </summary>
+    private Action IntervalInputChanged { get; set; } = delegate { };
+    // ------------------------------------------
+
+    #endregion Input Fields
 
     // ==============================================
 
     #region Constructor
 
-    public SettingsViewModel()
+    public SettingsViewModel(IStringLocalizer localizer,
+        IOptions<AppConfig> appinfo,
+        INavigator navigator,
+        IWritableOptions<TimeInputConfig> config)
     {
-        //    IStringLocalizer localizer,
-        //    IOptions<AppConfig> appInfo,
-        //    INavigator navigator,
-        //    IWritableOptions<TimeInputConfig> config
-        //)
-        //{
-        //    this.localizer = localizer;
-        //    this.appConfig = appInfo;
-        //    this.navigator = navigator;
-        //    this.timeInputConfig = config;
+        this.localizer = localizer;
+        this.appConfig = appinfo;
+        this.navigator = navigator;
+        this.timeInputConfig = config;
+
+        StartTime = timeInputConfig.Value.StartTime;
+        EndTime = timeInputConfig.Value.EndTime;
+        Interval = timeInputConfig.Value.Interval;
+
+        IntervalInputChanged += OnIntervalInputChanged;
     }
 
     #endregion Constructor
 
     // ==============================================
+
+    #region Event Listeners
+
+    /// <summary> Fires when the hour field of the interval input setting changes. </summary>
+    /// <param name="value"> The new value. </param>
+    partial void OnIntervalHourFieldChanged(int value)
+    {
+        IntervalInputChanged?.Invoke();
+    }
+
+    /// <summary> Fires when the hour field of the interval minute setting changes. </summary>
+    /// <param name="value"> The new value. </param>
+    partial void OnIntervalMinuteFieldChanged(int value)
+    {
+        IntervalInputChanged?.Invoke();
+    }
+
+    /// <summary> Fires when the second field of the interval input setting changes. </summary>
+    /// <param name="value"> The new value. </param>
+    partial void OnIntervalSecondFieldChanged(int value)
+    {
+        IntervalInputChanged?.Invoke();
+    }
+
+    /// <summary>
+    /// Sets the saved interval to a TimeSpan created from the interval input fields when their
+    /// contents change.
+    /// </summary>
+    private void OnIntervalInputChanged()
+    {
+        Interval = ConstructIntervalObject();
+        SaveIntervalPeriod(Interval);
+    }
+
+    #endregion Event Listeners
+
+    // ==============================================
+
+    #region Setting Processing
+
+    /// <summary> Saves the start time to the app configuration. </summary>
+    /// <param name="newTime"> The new time. </param>
+    private void SaveStartTime(TimeSpan newTime)
+    {
+        timeInputConfig.UpdateAsync(timeInputConfig => timeInputConfig with { StartTime = newTime });
+    }
+
+    /// <summary> Saves a new end time to the app configuration. </summary>
+    /// <param name="newTime"> The new time. </param>
+    private void SaveEndTime(TimeSpan newTime)
+    {
+        timeInputConfig.UpdateAsync(timeInputConfig => timeInputConfig with { EndTime = newTime });
+    }
+
+    /// <summary> Saves an interval period to the app configuration.. </summary>
+    /// <param name="newPeriod"> The new value of Interval. </param>
+    private void SaveIntervalPeriod(TimeSpan newPeriod)
+    {
+        timeInputConfig.UpdateAsync(timeInputConfig => timeInputConfig with { Interval = newPeriod });
+    }
+
+    /// <summary> Constructs a new Timespan object from the interval input fields. </summary>
+    /// <returns> A TimeSpan representation of the interval. </returns>
+    private TimeSpan ConstructIntervalObject()
+    {
+        return new TimeSpan(IntervalHourField, IntervalMinuteField, IntervalSecondField);
+    }
+
+    #endregion Setting Processing
 }
